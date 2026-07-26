6c58de155e0bcef13a348f63173d045823a3854b
Alistair Spark <aspark21@users.noreply.github.com>
2026-07-26 23:23:47 +0100

    Skip MDL-86832 upgrade

--- public/lib/db/upgrade.php
+++ public/lib/db/upgrade.php
@@ -1691,11 +1691,7 @@ function xmldb_main_upgrade($oldversion) {
     if ($oldversion < 2026011600.01) {
         // Remove activity_modules block.
 
-        if (!file_exists($CFG->dirroot . "/blocks/activity_modules/version.php")) {
-            uninstall_plugin('block', 'activity_modules');
-            // Delete all the admin preset plugin references to activity_modules.
-            $DB->delete_records('adminpresets_plug', ['plugin' => 'block', 'name' => 'activity_modules']);
-        }
+
 
         // Main savepoint reached.
         upgrade_main_savepoint(true, 2026011600.01);
